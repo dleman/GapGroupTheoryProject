2375c0efac1c53978512e6a0ee9d28fd32e97fa7
dleman <dawid.leman@onet.pl>
2023-06-08 20:11:27 +0200

    generators + saving to file

--- main.py
+++ main.py
@@ -25,16 +25,23 @@ if __name__ == "__main__":
     rand1 = random.randrange(2, 14, 2)
     rand2 = random.randrange(2, 14, 2)
     statements = [
-        "local G, H;",
+        "local G, H, G_generators, H_generators;",
        f"G := SymmetricGroup({rand1});",
-       f"H := SymmetricGroup({rand2});",
+       f"H := DihedralGroup(IsPermGroup, {rand2});",
       """Print("G: ", G, "\\n");""",
       """Print("H: ", H, "\\n");""",
       """if IsSubgroup(G, H) then
             Print("H is a subgroup of G.\\n");
         else
             Print("H is not a subgroup of G.\\n");
-    fi;"""]
+    fi;""",
+    """""",
+    """G_generators := GeneratorsOfGroup(G);""",
+    """H_generators := GeneratorsOfGroup(H);""",
+    """Print("G_generators: ", G_generators, "\\n");""",
+    """Print("H_generators: ", H_generators, "\\n");"""
+    ]
+
     simulation = GapFunction("Simulate", statements)
 
     gap_file_name = "simulation.g"
@@ -52,4 +59,7 @@ if __name__ == "__main__":
     with open(sh_file_name, "w") as file:
         file.write(str(sh))
     script_path = f"./{sh_file_name}"
-    subprocess.run(["sh", script_path])
+
+    result_file_name = "gap_output.txt"
+    with open(result_file_name, "w") as file:
+        subprocess.run(["sh", script_path], stdout=file)
