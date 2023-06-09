d1061487338917c6f178eb505e884297cf2eedfe
dleman <dawid.leman@onet.pl>
2023-06-09 14:57:11 +0200

    Group generation check + orbit + isomorphism

--- main.py
+++ main.py
@@ -25,21 +25,34 @@ if __name__ == "__main__":
     rand1 = random.randrange(2, 14, 2)
     rand2 = random.randrange(2, 14, 2)
     statements = [
-        "local G, H, G_generators, H_generators;",
+        "local G, H, G_generators, H_generators, G_group_check, H_group_check, G_orbit, isomorphism;",
        f"G := SymmetricGroup({rand1});",
        f"H := DihedralGroup(IsPermGroup, {rand2});",
-      """Print("G: ", G, "\\n");""",
-      """Print("H: ", H, "\\n");""",
-      """if IsSubgroup(G, H) then
-            Print("H is a subgroup of G.\\n");
-        else
-            Print("H is not a subgroup of G.\\n");
+        """Print("G: ", G, "\\n");""",
+        """Print("H: ", H, "\\n");""",
+        """if IsSubgroup(G, H) then
+        Print("H is a subgroup of G.\\n");
+    else
+        Print("H is not a subgroup of G.\\n");
     fi;""",
-    """""",
-    """G_generators := GeneratorsOfGroup(G);""",
-    """H_generators := GeneratorsOfGroup(H);""",
-    """Print("G_generators: ", G_generators, "\\n");""",
-    """Print("H_generators: ", H_generators, "\\n");"""
+        """# Define the generators for groups and create new groups for check""",
+        """G_generators := GeneratorsOfGroup(G);""",
+        """H_generators := GeneratorsOfGroup(H);""",
+        """Print("G_generators: ", G_generators, "\\n");""",
+        """Print("H_generators: ", H_generators, "\\n");""",
+        """# Define the group orbit to test creating new SymmetricGroup""",
+        """G_orbit := Orbits(G)[1];""",
+        """G_group_check := SymmetricGroup(G_orbit);""",
+        """H_group_check := Group(H_generators);""",
+        """# Check group Isomorphism""",
+        """isomorphism := IsomorphismGroups(G, H);;""",
+        """
+        if isomorphism <> fail then
+        Print("G and H are isomorphic.\\n");
+        Print("Isomorphism: ", isomorphism, "\\n");
+    else
+        Print("G and H are not isomorphic.\\n");
+    fi;"""
     ]
 
     simulation = GapFunction("Simulate", statements)
